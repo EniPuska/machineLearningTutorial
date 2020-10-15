8cbfaf1a069b5cc065a0297cec712a15047b8afc
EniPuska <eni.puska@gmail.com>
2020-10-15 14:12:57 +0300

    ....

--- mulitple-linear-regression/multiple-linear-regression.py
+++ mulitple-linear-regression/multiple-linear-regression.py
@@ -5,7 +5,7 @@ Multiple Linear Regression
 @author: alex-dev
 """
 
-#import numpy as np
+import numpy as np
 #import matplotlib.pyplot as plt
 import pandas as pd
 #from sklearn.impute import SimpleImputer 
@@ -13,9 +13,10 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
 
 #import the dataset
-#windows 
+#windows                                                                         
 dataset = pd.read_csv("C:\\Users\\alex-dev\\Desktop\\projects\\machine-learning\\mulitple-linear-regression\\50_Startups.csv")
 #Linux
 #dataset = pd.read_csv("./Data.csv")
@@ -44,4 +45,27 @@ regressor = LinearRegression()
 regressor.fit(xTrain, yTrain)
 
 #Predict the Test set results
-yPrediction = regressor.predict()
+yPrediction = regressor.predict(xTest)
+
+#Building the optimal model
+
+x = np.append(arr = np.ones((50, 1)).astype(int), values = x, axis = 1)
+xOpt = np.array(x[:, [0, 1, 2, 3, 4, 5]], dtype=float)
+regressorOLS = sm.OLS(endog = y, exog=xOpt).fit();
+regressorOLS.summary();
+#remove the second column
+xOpt = np.array(x[:, [0, 1, 3, 4, 5]], dtype=int)
+regressorOLS = sm.OLS(endog = y, exog=xOpt).fit();
+regressorOLS.summary();
+#remove the first column
+xOpt = np.array(x[:, [0, 3, 4, 5]], dtype=int)
+regressorOLS = sm.OLS(endog = y, exog=xOpt).fit();
+regressorOLS.summary();
+#remove the forth column
+xOpt = np.array(x[:, [0, 3, 5]], dtype=int)
+regressorOLS = sm.OLS(endog = y, exog=xOpt).fit();
+regressorOLS.summary();
+#remove the fifth column
+xOpt = np.array(x[:, [0, 3]], dtype=int)
+regressorOLS = sm.OLS(endog = y, exog=xOpt).fit();
+regressorOLS.summary();
